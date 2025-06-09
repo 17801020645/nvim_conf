a163d20f2dfe4321bc081cd638d6d9bf29b19386
yuhu <17801020645@163.com>
2025-06-09 16:27:10 +0800

    config neovide

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -30,3 +30,10 @@ vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("v", "<C-s>", "<Esc>:w<CR>")
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
 vim.keymap.set("n", "<C-a>", "ggVG", { silent = true })
+
+-- config neovide
+if vim.g.neovide then
+  vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+  vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,23 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- neovide config
+if vim.g.neovide then
+  --  vim.o.guifont = "CaskaydiaCove Nerd Font:h18"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_scale_factor = 0.9
+  -- turn off animate becuase we are using neovide
+  vim.g.snacks_animate = false
+end
+
+-- opt.wrap = true
+-- opt.spelllang = { "en", "cjk" }
+-- opt.linebreak = false
+
+-- vim.g.root_spec = { "cwd" }
+
+-- vim.g.lazyvim_prettier_needs_config = false
+
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "pyright"
